9c47ea0f07115daf1d75ba734862d2c0083e1fea
Manasa15ch <156450174+Manasa15ch@users.noreply.github.com>
2025-04-28 21:21:41 +0530

    Update categories.component.js

--- categories.component.js
+++ categories.component.js
@@ -3,9 +3,18 @@
   
   angular.module('MenuApp')
   .component('categories', {
-    templateUrl: 'templates/categories.template.html',
+    templateUrl: 'templates/categories.component.template.html',
     bindings: {
       categories: '<'
-    }
+    },
+    controller: CategoriesComponentController
   });
+  
+  function CategoriesComponentController() {
+    var $ctrl = this;
+    
+    $ctrl.$onInit = function() {
+      console.log('Categories in component:', $ctrl.categories);
+    };
+  }
 })();
