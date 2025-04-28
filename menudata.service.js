5272801b521a34320ef4cbd53a1e50e0a3d489d7
Manasa15ch <156450174+Manasa15ch@users.noreply.github.com>
2025-04-28 21:12:12 +0530

    Update menudata.service.js

--- menudata.service.js
+++ menudata.service.js
@@ -1,23 +1,28 @@
-(function () {
+(function() {
   'use strict';
+  
   angular.module('data')
-    .service('MenuDataService', MenuDataService);
-
+  .service('MenuDataService', MenuDataService);
+  
   MenuDataService.$inject = ['$http'];
   function MenuDataService($http) {
     var service = this;
-
-    service.getAllCategories = function () {
+    
+    service.getAllCategories = function() {
       return $http({
-        method: 'GET',
-        url: 'https://coursera-jhu-default-rtdb.firebaseio.com/categories.json'
+        method: "GET",
+        url: "https://coursera-jhu-default-rtdb.firebaseio.com/categories.json"
+      }).then(function(response) {
+        return response.data;
       });
     };
-
-    service.getItemsForCategory = function (categoryShortName) {
+    
+    service.getItemsForCategory = function(categoryShortName) {
       return $http({
-        method: 'GET',
-        url: `https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/${categoryShortName}.json`
+        method: "GET",
+        url: "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/" + categoryShortName + ".json"
+      }).then(function(response) {
+        return response.data;
       });
     };
   }
