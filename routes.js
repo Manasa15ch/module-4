6ed6ef0dc87b2ef41d841954642af463b66a2356
Manasa15ch <156450174+Manasa15ch@users.noreply.github.com>
2025-04-28 21:13:18 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,42 +1,62 @@
-(function () {
+(function() {
   'use strict';
+  
   angular.module('MenuApp')
-    .config(RoutesConfig);
-
+  .config(RoutesConfig);
+  
   RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
   function RoutesConfig($stateProvider, $urlRouterProvider) {
+    
+    // Redirect to home page if no other URL matches
     $urlRouterProvider.otherwise('/');
-
+    
+    // Set up UI states
     $stateProvider
       .state('home', {
         url: '/',
-        templateUrl: 'home.template.html'
+        templateUrl: 'templates/home.template.html'
       })
       .state('categories', {
         url: '/categories',
-        templateUrl: 'categories.template.html',
-        controller: 'CategoriesController as ctrl',
+        templateUrl: 'templates/categories.template.html',
+        controller: 'CategoriesController as categoriesCtrl',
         resolve: {
-          categories: ['MenuDataService', function (MenuDataService) {
-            return MenuDataService.getAllCategories()
-              .then(function (response) {
-                return response.data;
-              });
+          categories: ['MenuDataService', function(MenuDataService) {
+            return MenuDataService.getAllCategories();
           }]
         }
       })
       .state('items', {
         url: '/items/{categoryShortName}',
-        templateUrl: 'items.template.html',
-        controller: 'ItemsController as ctrl',
+        templateUrl: 'templates/items.template.html',
+        controller: 'ItemsController as itemsCtrl',
         resolve: {
-          items: ['$stateParams', 'MenuDataService', function ($stateParams, MenuDataService) {
-            return MenuDataService.getItemsForCategory($stateParams.categoryShortName)
-              .then(function (response) {
-                return response.data.menu_items;
-              });
+          items: ['$stateParams', 'MenuDataService', 
+                  function($stateParams, MenuDataService) {
+            return MenuDataService.getItemsForCategory($stateParams.categoryShortName);
           }]
         }
       });
+  }
+  
+  // Categories Controller
+  angular.module('MenuApp')
+  .controller('CategoriesController', CategoriesController);
+  
+  CategoriesController.$inject = ['categories'];
+  function CategoriesController(categories) {
+    var ctrl = this;
+    ctrl.categories = categories;
+  }
+  
+  // Items Controller
+  angular.module('MenuApp')
+  .controller('ItemsController', ItemsController);
+  
+  ItemsController.$inject = ['items'];
+  function ItemsController(items) {
+    var ctrl = this;
+    ctrl.items = items.menu_items;
+    ctrl.category = items.category;
   }
 })();
