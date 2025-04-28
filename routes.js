5ca725e63b43cb20cde15086a5da3c809ead0bdc
Manasa15ch <156450174+Manasa15ch@users.noreply.github.com>
2025-04-28 21:25:49 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -3,13 +3,14 @@ function CategoriesController(categories) {
   var ctrl = this;
   console.log('Categories data received:', categories);
   
-  // Ensure categories is always an array
-  ctrl.categories = categories || [];
+  // The API returns an array, so we can use it directly
+  ctrl.categories = categories;
   
-  // If the API returns an object, convert it to array
+  // If for some reason it's not an array, convert it
   if (categories && !Array.isArray(categories)) {
     ctrl.categories = Object.keys(categories).map(function(key) {
       return categories[key];
     });
   }
 }
+
