500b6ee073b1976163bfc8e8bb04c6c038fd2ea6
Manasa15ch <156450174+Manasa15ch@users.noreply.github.com>
2025-05-31 10:50:49 +0530

    menuapp.module.js

--- menuapp.module.js
+++ menuapp.module.js
@@ -1,5 +1,8 @@
-(function() {
-  'use strict';
-  
-  angular.module('MenuApp', ['ui.router', 'data']);
+(function () {
+'use strict';
+
+angular.module('MenuApp', ['ui.router', 'data']);
+
 })();
+
+
