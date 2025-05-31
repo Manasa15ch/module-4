0d0521c95207360525a8139a17ca57a9260474d7
Manasa15ch <156450174+Manasa15ch@users.noreply.github.com>
2025-05-31 10:54:15 +0530

    Update categories.component.js

--- categories.component.js
+++ categories.component.js
@@ -1,26 +1,12 @@
-(function() {
-  'use strict';
-  
-  angular.module('MenuApp')
-  .component('categories', {
-    templateUrl: 'templates/categories.component.template.html',
-    bindings: {
-      categories: '<'
-    },
-    controller: CategoriesComponentController
-  });
-  
-  function CategoriesComponentController() {
-    var $ctrl = this;
-    
-    $ctrl.$onInit = function() {
-      console.log('Categories in component:', $ctrl.categories);
-    };
-    
-    $ctrl.$onChanges = function(changes) {
-      if (changes.categories) {
-        console.log('Categories changed:', $ctrl.categories);
-      }
-    };
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.component('categories', {
+  templateUrl: 'categories.template.html',
+  bindings: {
+    items: '<'
   }
+});
+
 })();
