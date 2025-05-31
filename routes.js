d92da718a59e78496b5df9762a6dd95611f6d1a8
Manasa15ch <156450174+Manasa15ch@users.noreply.github.com>
2025-05-31 10:58:54 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,16 +1,42 @@
-CategoriesController.$inject = ['categories'];
-function CategoriesController(categories) {
-  var ctrl = this;
-  console.log('Categories data received:', categories);
-  
-  // The API returns an array, so we can use it directly
-  ctrl.categories = categories;
-  
-  // If for some reason it's not an array, convert it
-  if (categories && !Array.isArray(categories)) {
-    ctrl.categories = Object.keys(categories).map(function(key) {
-      return categories[key];
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('home', {
+      url: '/',
+      template: '<h1>Welcome to our Restaurant</h1><a ui-sref="categories">View Categories</a>'
+    })
+
+    .state('categories', {
+      url: '/categories',
+      template: '<categories items="categories.items"></categories>',
+      controller: 'CategoriesController as categories',
+      resolve: {
+        items: ['MenuDataService', function (MenuDataService) {
+          return MenuDataService.getAllCategories();
+        }]
+      }
+    })
+
+    .state('items', {
+      url: '/items/{category}',
+      template: '<items items="items.items"></items>',
+      controller: 'ItemsController as items',
+      resolve: {
+        items: ['$stateParams', 'MenuDataService',
+          function ($stateParams, MenuDataService) {
+            return MenuDataService.getItemsForCategory($stateParams.category);
+        }]
+      }
     });
-  }
 }
 
+})();
